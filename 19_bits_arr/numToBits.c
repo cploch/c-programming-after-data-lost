e0182d1df761792385177e20c7fc13b6eddb5afc
Coursera Learner <nobody@nowhere.nul>
2020-07-12 23:43:53 +0000

    submitting ass 19

--- 19_bits_arr/numToBits.c
+++ 19_bits_arr/numToBits.c
@@ -23,7 +23,7 @@ int getNthBit(uint32_t number, int bit) {
  *   of "nums".  Note that each number in "nums" will results in 32
  *   bits in "bits".  If this is not true, your function should
  *   print a message with the format:
- *	"Invalid call to numToBits! nBits is %d, nNums is %d\n",
+ *"Invalid call to numToBits! nBits is %d, nNums is %d\n",
  *   (where the first %d is nBits, and the second %d is nNums)
  *   then return without doing anything else.
  *
@@ -36,7 +36,15 @@ int getNthBit(uint32_t number, int bit) {
  *   be bit 30 of nums[0], and so on.
  */
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
-
+  if (nBits < nNums*32) {
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n", nBits, nNums);
+    return;
+  }
+  for (int i = 0; i < nNums; i++) {
+    for (int j = 0; j < 32; j ++) {
+      bits[32*i + j] = getNthBit(nums[i], (31 - j));
+    }
+  }
 }
 
 void doTest(uint32_t * nums, int n) {
