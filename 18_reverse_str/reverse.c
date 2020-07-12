6e366cdf311d70325bb30e299cdd4645631a6e2b
Coursera Learner <nobody@nowhere.nul>
2020-07-12 23:41:05 +0000

    submitting ass 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,11 +3,26 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  if (str == NULL){
+    printf("Encountered a null pointer, returning from reverse function\n");
+    return;
+  }
+  int count = 0;
+  while (str[count] != '\0') {
+    count++; //count is size of string (not including null terminator, except when empty)
+  }
+  int max = count - 1; //max is highest index reached, valid for non-empty strings
+  char str_new[count + 1]; //declare new string that is equal to size of "str" including null terminator
+  str_new[count] = '\0';
+  for (int i = 0; i < count; i++){ //in empty case the for-loop is not entered
+    str_new[i] = str[max - i];
+  }
+  strncpy(str, str_new, (count+1));
 }
 
 int main(void) {
   char str0[] = "";
+  // char * str0 = NULL;
   char str1[] = "123";
   char str2[] = "abcd";
   char str3[] = "Captain's log, Stardate 42523.7";
