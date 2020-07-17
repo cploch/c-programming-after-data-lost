0f41737f3bdbe06219c3570284ca33cbf2ddd91a
Coursera Learner <nobody@nowhere.nul>
2020-07-17 09:20:22 +0000

    Submitting ass 29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,15 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * outFileName = malloc((strlen(inputName) + 8) * sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+  return outFileName;
 }
+
+/*
+//outfileNAme is argv[2] + ".txt", so add 4 to its length.
+char * outFileName = malloc((strlen(argv[2]) + 5) * sizeof(*outFileName)); //Mistake #1: space for null terminator was not malloced, so I changed "4" to "5".
+strcpy(outFileName, argv[2]);
+strcat(outFileName, ".enc");
+*/
