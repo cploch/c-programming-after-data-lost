b01f5c0cc8587b1e1cc3c0356b1ba6bf86b8217a
Coursera Learner <nobody@nowhere.nul>
2020-07-23 09:31:25 +0000

    Submitting ass 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -4,14 +4,60 @@
 #include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * answer = malloc(sizeof(*answer));
+  answer->size = 0;
+  answer->array = NULL;
+  return answer;
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  int found = 0;
+  if (name == NULL){
+    name = "NULL";
+  }
+  for (int i = 0; i < c->size; i++){
+    int result = strcmp(name, c->array[i]->str);
+    if (result == 0){
+      c->array[i]->count++;
+      found = 1;
+    }
+  }
+  if (found == 0){
+    c->size++;
+    c->array = realloc(c->array, sizeof(*c->array)*(c->size));
+    c->array[c->size-1] = malloc(sizeof(*c->array[c->size-1]));
+    c->array[c->size-1]->str = malloc((strlen(name)+1));
+    strcpy(c->array[c->size-1]->str, name);
+    c->array[c->size-1]->count = 1;
+  }                
+  
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  int foundNull = 0;
+  int indexNull = 0;
+  int resultNull = 0;
+  for (int i = 0; i < c->size; i++){
+    resultNull = strcmp(c->array[i]->str, "NULL");
+    if (resultNull == 0){
+      foundNull = 1;
+      indexNull = i;
+    }
+    else{
+      fprintf(outFile, "%s: %d\n", c->array[i]->str, c->array[i]->count);
+    }
+  }
+  if (foundNull == 1){
+    fprintf(outFile, "<unknown>: %d\n", c->array[indexNull]->count);
+  }
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for (int i = 0; i < c->size; i++){
+    free(c->array[i]->str);
+    free(c->array[i]);
+  }
+  free(c->array);
+  free(c);
 }
--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -2,12 +2,15 @@
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  char * str;
+  int count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** array;
+  int size;
 };
 typedef struct _counts_t counts_t;
 
