d0a690743d60760e6903e487826f421acbd61c6b
Coursera Learner <nobody@nowhere.nul>
2020-07-24 05:19:17 +0000

    Added error checking in Ass 34 to detect fewer than 2 arguments.

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -31,6 +31,10 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc < 3){
+    fprintf(stderr, "Error: There are fewer than 2 arguments. There must be at least 2.\n");
+    return EXIT_FAILURE;
+  }
   kvarray_t * kv = readKVs(argv[1]);
  //count from 2 to argc (call the number you count i)
   for (int i = 2; i < argc; i++){
